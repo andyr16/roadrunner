e3960dd47ea71bf74a51c44a33996edc676a901d
Andy Rihn <andrewrihn@gmail.com>
2014-04-03 22:19:41 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Roadrunner::Application.config.secret_key_base = 'b09d4e2cc6c7f6c6e7ce946242c781e5996b15849bf4764a353f930f1acf4a32bb762a7664d038f1f8be50fdb59e5f605d5f6c5b42862ef0eba8f5aba367f30e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Roadrunner::Application.config.session_store :cookie_store, key: '_roadrunner_session'
