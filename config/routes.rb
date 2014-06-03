470b1d65fac1a817109ead9fce72335d51da9f99
Andy Rihn <andrewrihn@gmail.com>
2014-06-02 21:35:09 -0400

    added contact us page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Roadrunner::Application.routes.draw do
   devise_for :users
   root 'pages#home'
   get 'about' => 'pages#about'
+  get 'services' => 'pages#services'
+  get 'contact_roadrunner' => 'pages#contact_roadrunner'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
